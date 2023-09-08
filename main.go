a6cfc8a57907670ee3cf79e9f4e2bcaae3469f85
GGermanBoldyrev <boldyrev_german@mail.ru>
2023-09-08 17:16:13 +0300

    pointers added

--- main.go
+++ main.go
@@ -23,11 +23,11 @@ type Settings struct {
 }
 
 type Player struct {
-	ID        uint16    `json:"id"`
-	Name      string    `json:"name"`
-	Team      string    `json:"team"`
-	Grahphics Grahphics `json:"grahphics"`
-	Settings  Settings  `json:"settings"`
+	ID        uint16     `json:"id"`
+	Name      string     `json:"name"`
+	Team      string     `json:"team"`
+	Grahphics *Grahphics `json:"grahphics"`
+	Settings  *Settings  `json:"settings"`
 }
 
 var players = []Player{
@@ -35,14 +35,14 @@ var players = []Player{
 		ID:   0,
 		Name: "Xantares",
 		Team: "Eternal Fire",
-		Grahphics: Grahphics{
+		Grahphics: &Grahphics{
 			Resolution:       "1024x720",
 			Shadow:           "Very Low",
 			AntiAliasing:     "4x MSAA",
 			Fxaa:             "Disabled",
 			TextureFiltering: "Anisotropic 4x",
 		},
-		Settings: Settings{
+		Settings: &Settings{
 			Edpi:      800,
 			Crosshair: "cl_crosshair_drawoutline 0; cl_crosshairalpha 255; cl_crosshaircolor 1; cl_crosshaircolor_b 50; cl_crosshaircolor_g 250; cl_crosshaircolor_r 50; cl_crosshairdot 0; cl_crosshairgap 0; cl_crosshairsize 3; cl_crosshairstyle 4; cl_crosshairthickness 0.5; cl_crosshair_sniper_width 1;",
 			Viewmodel: "viewmodel_fov 60; viewmodel_offset_x 1; viewmodel_offset_y 1; viewmodel_offset_z -1; viewmodel_presetpos 1; cl_viewmodel_shift_left_amt 1.5; cl_viewmodel_shift_right_amt 0.75; viewmodel_recoil 0; cl_righthand 1;",
@@ -53,14 +53,14 @@ var players = []Player{
 		ID:   1,
 		Name: "Niko",
 		Team: "G2 Esports",
-		Grahphics: Grahphics{
+		Grahphics: &Grahphics{
 			Resolution:       "1152x864",
 			Shadow:           "High",
 			AntiAliasing:     "8x MSAA",
 			Fxaa:             "Disabled",
 			TextureFiltering: "Anisotropic 8x",
 		},
-		Settings: Settings{
+		Settings: &Settings{
 			Edpi:      540,
 			Crosshair: "cl_crosshair_drawoutline 0; cl_crosshairalpha 255; cl_crosshaircolor 5; cl_crosshaircolor_b 255; cl_crosshaircolor_g 255; cl_crosshaircolor_r 255; cl_crosshairdot 0; cl_crosshairgap -4; cl_crosshairsize 1; cl_crosshairstyle 4; cl_crosshairthickness 1; cl_crosshair_sniper_width 1;",
 			Viewmodel: "viewmodel_fov 68; viewmodel_offset_x 2.5; viewmodel_offset_y 0; viewmodel_offset_z -1.5; viewmodel_presetpos 3; cl_viewmodel_shift_left_amt 1.5; cl_viewmodel_shift_right_amt 0.75; viewmodel_recoil 0; cl_righthand 1;",
@@ -71,14 +71,14 @@ var players = []Player{
 		ID:   2,
 		Name: "Ropz",
 		Team: "FaZe Clan",
-		Grahphics: Grahphics{
+		Grahphics: &Grahphics{
 			Resolution:       "1920x1080",
 			Shadow:           "Very Low",
 			AntiAliasing:     "4x MSAA",
 			Fxaa:             "Disabled",
 			TextureFiltering: "Anisotropic 4x",
 		},
-		Settings: Settings{
+		Settings: &Settings{
 			Edpi:      708,
 			Crosshair: "cl_crosshair_drawoutline 0; cl_crosshairalpha 255; cl_crosshaircolor 1; cl_crosshaircolor_b -1000; cl_crosshaircolor_g 0; cl_crosshaircolor_r -1000; cl_crosshairdot 0; cl_crosshairgap -3; cl_crosshairsize 2; cl_crosshairstyle 4; cl_crosshairthickness 0; cl_crosshair_sniper_width 1;",
 			Viewmodel: "viewmodel_fov 68; viewmodel_offset_x 2.5; viewmodel_offset_y 0; viewmodel_offset_z -1.5; viewmodel_presetpos 3; cl_viewmodel_shift_left_amt 0.5; cl_viewmodel_shift_right_amt 0.25; viewmodel_recoil 0; cl_righthand 1;",
